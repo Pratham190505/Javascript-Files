ab71fca9f8b96d300dd5df6ab0c000b3042e8157
Pratham190505 <prathampawar005@gmail.com>
2025-01-03 14:30:22 +0000

    Datatypes

---
+++ Datatypes.js
@@ -0,0 +1,23 @@
+//Primitive Datatypes:- String Symbol Boolean BigInt null Number undefined
+// const score = 100;
+// const scoreValue = 100.3;
+
+// const isLoggedIn = false;
+
+// const outsideTemp = null;
+
+// const id = Symbol('123');
+// const anotherID = Symbol('123');
+// const bignumber = 3125697521402842n;
+//References(Non-primitive Data-types):- Arrays Objects Functions 
+
+const arr = ["Pratham", "Vraj","Het"];
+const obj = {
+    name : "Pratham",
+    RollNo : 11
+};
+const myFunction = function(){
+    console.log("Pratham")
+};
+console.log( typeof myFunction);
+
