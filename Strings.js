374e01ce1ef30275933d569c7b20281d723b6ad8
Pratham190505 <prathampawar005@gmail.com>
2025-01-04 14:21:02 +0000

    Strings

---
+++ Strings.js
@@ -0,0 +1,18 @@
+const name = "Pratham";
+const repoCount = "50";
+console.log(`My name is ${name} and repoCount is ${repoCount}`);
+const getName = new String('   pratham-pawar');   //Syntax to declare a string datatype
+// console.log(getName.toUpperCase());
+// console.log(getName.__proto__);
+// console.log(getName.charAt(2));
+// console.log(getName.indexOf('w'));
+//console.log(getName);
+
+// const newString = getName.substring(0,4);
+// console.log(newString);
+// const newString1 = getName.slice(-7,-5);
+// console.log(newString1);
+// const anotherString = getName.trim();
+// console.log(anotherString);
+const url = "https://pratham.com/pratham@pawar115";
+console.log(url.replace('@', '%'));
