d7e2d3ecdccf8596287afaffbe99c343bd044acd
Pratham190505 <prathampawar005@gmail.com>
2025-01-06 15:13:43 +0000

    NumberAndMath

---
+++ NumberAndMath.js
@@ -0,0 +1,26 @@
+// const score = 400;
+// console.log(score);
+// const balance = new Number(100);
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3));
+
+// const otherNumber = 12.8966;
+// console.log(otherNumber.toPrecision(4));
+
+// const hundreds = 100000;
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN'));
+//************************MATH*******************
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(5.3));
+// console.log(Math.ceil(5.3));
+// console.log(Math.floor(5.3));
+// console.log(Math.sqrt(36));
+
+console.log((Math.random() * 10) + 1);
+console.log(Math.floor((Math.random() * 10) + 1));
+let max = 20;
+let min = 10;
+console.log(Math.floor((Math.random() * (max - min + 1)) + min));
