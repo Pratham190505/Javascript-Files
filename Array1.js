3bf21b1fb566b31407ef19fb5ab26eaebaa42733
Pratham190505 <prathampawar005@gmail.com>
2025-01-07 16:48:45 +0000

    Array1

---
+++ Array1.js
@@ -0,0 +1,24 @@
+const arr = [1,2,3,4,5];
+console.log(arr[0]);
+const newarr = new Array("Pratham", "Mrudul");
+// arr.push(7);
+// arr.push(12);
+// arr.pop();
+// arr.unshift(45);
+// arr.shift();
+// console.log(arr.includes(3));
+// console.log(arr.indexOf(2));
+// const arr1 = arr.join();
+// console.log(typeof arr1);
+console.log("A", arr);
+const arr1 = arr.slice(1,3);
+console.log(arr1);
+console.log("B", arr);
+const arr2 = arr.splice(1,3);
+console.log(arr2);
+console.log(arr);  //splice will also remove the elements from the main array...
+
+
+
+
+
