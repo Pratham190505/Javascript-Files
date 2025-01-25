952795eda802afa0f6b2726a3e932859054d05ca
Pratham190505 <prathampawar005@gmail.com>
2025-01-25 13:39:52 +0000

    ScopelevelAndMiniHoisting

--- ScopelevelAndMiniHoisting.js
+++ ScopelevelAndMiniHoisting.js
@@ -7,3 +7,24 @@ function one(){
     two();
 }
 one();
+if(true)
+{
+    const username = "Pratham";
+    if(username === "Pratham")
+    {
+        const website = " youtube";
+        console.log(username + website);
+    }
+    //console.log(website);
+}
+//console.log(username);
+
+addOne(5);
+function addOne(num){
+    return num+1;
+}
+addTwo(10);
+
+const addTwo = function(num){
+    return num+2;
+}
