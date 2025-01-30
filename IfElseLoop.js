3108eb9730e11950928938e0246e1b7b04c4cc2f
Pratham190505 <prathampawar005@gmail.com>
2025-01-30 17:01:56 +0000

    IfElseLoop

---
+++ IfElseLoop.js
@@ -0,0 +1,19 @@
+// const balance = 700;
+// if(balance < 500)
+// {
+//     console.log("less than 500");
+// }
+// else if(balance < 750)
+// {
+//     console.log("less than 750");
+// }
+// else{
+//     console.log("less than 1200");
+// }
+
+const userLoggedIn = true;
+const debitCard = true;
+if(userLoggedIn && debitCard)
+{
+    console.log("executed");
+}
